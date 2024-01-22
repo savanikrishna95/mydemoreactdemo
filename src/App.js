262d0abdf074542fad45c5a6ec356fcdca99e59d
savanikrishna95 <savanitina@outlook.com>
2024-01-22 17:50:59 +0530

    added code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload xzxzx. 
+          Edit <code>src/App.js</code> and save to reload krisjna. 
         </p>
         <a
           className="App-link"
