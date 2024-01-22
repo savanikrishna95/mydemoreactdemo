84b545924d712c2c8258f0c44b39ede4c69e7eb2
savanikrishna95 <savanitina@outlook.com>
2024-01-23 00:28:07 +0530

    added code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { AppContainer } from 'react-hot-loader';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -9,8 +10,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 root.render(
+
   <React.StrictMode>
+      <AppContainer>
     <App />
+    </AppContainer>
   </React.StrictMode>
 );
 
