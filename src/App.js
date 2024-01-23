5082abf8b2fcf9ca0062f8389dbd1b90dae96043
savanikrishna95 <savanitina@outlook.com>
2024-01-23 15:16:58 +0530

    added code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> my name is krishna!!. 
+          Edit <code>src/App.js</code> my name is krishnadsdsdscxzdsad!!. 
         </p>
         <a
           className="App-link"
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 
 import App from './App';
@@ -18,7 +18,16 @@ root.render(
   </React.StrictMode>
 );
 
-
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('../mydemoreactdemo/service-worker.js')
+    .then(registration => {
+      console.log('Service Worker registered with scope:', registration.scope);
+    })
+    .catch(error => {
+      console.error('Service Worker registration failed:', error);
+    });
+}
 
 
 // If you want to start measuring performance in your app, pass a function
