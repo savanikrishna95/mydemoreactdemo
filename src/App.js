e504dd4f6922e602ed13b17256f81e622a3a3cb3
savanikrishna95 <savanitina@outlook.com>
2024-01-23 15:36:13 +0530

    added code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> my name is the krishna Krishna Radhe radhe!!. 
+          Edit <code>src/App.js</code> my name is the krishna!!. 
         </p>
         <a
           className="App-link"
