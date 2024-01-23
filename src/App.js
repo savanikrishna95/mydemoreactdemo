26a075ba042e8ce4099e6b8bf15f629e6f04addc
savanikrishna95 <savanitina@outlook.com>
2024-01-23 16:27:42 +0530

    adde code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> my name is the krishna savanireact developedsdsrs!!. 
+          Edit <code>src/App.js</code> my name is the krishna savanireact developer!!. 
         </p>
         <a
           className="App-link"
